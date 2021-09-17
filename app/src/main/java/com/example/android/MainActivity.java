e0705901f8c00f60c89a2f6b3209f8db19df9d6b
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-09-17 13:14:26 +0400

    lab1 toast added

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Console;
 import java.util.ArrayList;
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayAdapter<String> adapter;
     ArrayList<String> catNames;
+    private Context nContext;
+    private Typeface mTypeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +43,32 @@ public class MainActivity extends AppCompatActivity {
         Button btnClearSelection = (Button) findViewById(R.id.btnClear);
         Button btnTOST = (Button) findViewById(R.id.btnTOST);
 
+
         catNames = new ArrayList<>();
 
-        adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_multiple_choice, catNames);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,catNames){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Cast the list view each item as text view
+                TextView item = (TextView) super.getView(position,convertView,parent);
+
+                // Set the typeface/font for the current item
+                item.setTypeface(mTypeface);
+
+                // Set the list view item's text color
+                item.setTextColor(Color.parseColor("#DDADAF"));
+
+                // Set the item text style to bold
+                item.setTypeface(item.getTypeface(), Typeface.BOLD);
+
+                // Change the item text size
+                item.setTextSize(TypedValue.COMPLEX_UNIT_DIP,18);
+
+                // return the view
+                return item;
+            }
+        };
+
 
         listView.setAdapter(adapter);
 
@@ -70,15 +101,45 @@ public class MainActivity extends AppCompatActivity {
         btnTOST.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                CharSequence text = "";
+                for (int i = 0;i<listView.getCount();i++){
+                    if(listView.isItemChecked(i)){
+                        text += adapter.getItem(i) + " ";
+                    }
+                }
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
 
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
     protected void onRestoreInstanceState(Bundle outState) {
         if (outState != null) {
             catNames = (ArrayList<String>) outState.getStringArrayList("myKey");
-            adapter = new ArrayAdapter<>(this,
-                    android.R.layout.simple_list_item_multiple_choice, catNames);
+            adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,catNames){
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent){
+                    // Cast the list view each item as text view
+                    TextView item = (TextView) super.getView(position,convertView,parent);
+
+                    // Set the typeface/font for the current item
+                    item.setTypeface(mTypeface);
+
+                    // Set the list view item's text color
+                    item.setTextColor(Color.parseColor("#DDADAF"));
+
+                    // Set the item text style to bold
+                    item.setTypeface(item.getTypeface(), Typeface.BOLD);
+
+                    // Change the item text size
+                    item.setTextSize(TypedValue.COMPLEX_UNIT_DIP,18);
+
+                    // return the view
+                    return item;
+                }
+            };
             ListView listView = (ListView) findViewById(R.id.lvMain);
             listView.setAdapter(adapter);
         }
@@ -89,4 +150,5 @@ public class MainActivity extends AppCompatActivity {
         outState.putStringArrayList("myKey", catNames);
         super.onSaveInstanceState(outState);
     }
+
 }
